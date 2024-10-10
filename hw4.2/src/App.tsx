77d160b92cdc01c3472cd6a1cd8ac29584ca650f
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-10-10 19:05:26 +0200

    hw4.2 final

--- hw4.2/src/App.tsx
+++ hw4.2/src/App.tsx
@@ -1,28 +1,56 @@
 import { useState } from "react";
-//import './App.css'
+
+function getRndNum() {
+  return Math.round(Math.random() * 100);
+}
 
 function App() {
   const [count, setCount] = useState(0);
+  const [firstNum, setfirstNum] = useState(getRndNum());
+  const [secondNum, setSecondNum] = useState(getRndNum());
+  const [inputState, setInputState] = useState();
+
+  function checkAnswer(e: Event) {
+    e.preventDefault();
+    const curAnswer = document.querySelector("#answer");
+    if (curAnswer)
+      if (curAnswer.value == (firstNum + secondNum)) {
+        setInputState(false);
+        setCount((cur) => cur + 1);
+        curAnswer.value = "";
+        setfirstNum(getRndNum());
+        setSecondNum(getRndNum());
+        setInputState("");
+
+      }
+      else {
+        setInputState(true);
+      }
+  }
 
   return (
     <main
-      
       style={{
         width: "100%",
-        //maxWidth: "700px",
-        //
-        padding: "20px",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
       }}
     >
+      <h1>Math Quiz</h1>
       <article className="container">
-        <h2>Ваши очки: 0</h2>
-        <h3>1 + 1 = ?</h3>
-        <fieldset role="group">
-          <input name="antwort" placeholder="Введите ответ" />
-          <input type="submit" value="Проверить" />
-        </fieldset>
+        <h2>Ваши очки: {count}</h2>
+        <h3>{firstNum} + {secondNum} = ?</h3>
+        <form onSubmit={(e) => checkAnswer(e)} >
+          <fieldset role="group">
+            <input aria-invalid={inputState} id="answer" name="answer" placeholder="Введите ответ" />
+            <input type="submit" value="Проверить" />
+          </fieldset>
+        </form>
       </article>
-    </main>
+    </main >
   );
 }
 
