a87a7467212826f399a1cb297d8e55ad94718bce
Panchenko Serhii <panchenko.mobi@gmail.com>
2024-10-13 15:04:14 +0200

    hw5

--- hw5/src/App.tsx
+++ hw5/src/App.tsx
@@ -1,23 +1,48 @@
-import { useState } from "react";
-//import './App.css'
 import styles from "./App.module.css";
-import imgSpotify from './assets/spotify.png';
+import imgSpotify from "./assets/spotify.png";
 
+import AppleLogo from "./Logos";
 
 function App() {
   return (
     <div className={styles.container}>
-      <header>
+      <header className={styles.header}>
         <img src={imgSpotify} alt="" />
       </header>
-      <main>
-        <h1>LIFE IS WASTED ON THE LIVING</h1>
-        <p>Sign in</p>
-        <p>with</p>
-        <div>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
+      <main className={styles.main}>
+        <div className={styles.form}>
+          <h1>LIFE IS WASTED ON THE LIVING</h1>
+          <p>Sign in</p>
+          <p>with</p>
+          <div className={styles.buttons}>
+            <AppleLogo
+              logoName="apple"
+              className={styles.icon}
+              fillColor="#1ED760" // Цвет заливки SVG
+              backgroundColor="#0C0417" // Цвет фона контейнера
+              width="110"
+              height="110"
+              viewBox="0 0 110 110"
+            />
+            <AppleLogo
+              logoName="google"
+              className={styles.icon}
+              fillColor="#1ED760" // Цвет заливки SVG
+              backgroundColor="#0C0417" // Цвет фона контейнера
+              width="110"
+              height="110"
+              viewBox="0 0 110 110"
+            />
+            <AppleLogo
+              logoName="x"
+              className={styles.icon}
+              fillColor="#1ED760" // Цвет заливки SVG
+              backgroundColor="#0C0417" // Цвет фона контейнера
+              width="110"
+              height="110"
+              viewBox="0 0 110 110"
+            />
+          </div>
         </div>
       </main>
     </div>
--- hw5/vite.config.ts
+++ hw5/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    svgr({
+      svgrOptions: {
+        exportType: 'named', // Устанавливаем экспорт как именованный
+      },
+    }),
+    react(),
+  ],
+  base: "/react-home-work/hw4.2/dist/",
+});
