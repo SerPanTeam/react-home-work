88b507afb9f9b7c6e4865274bf47c87107b57015
Panchenko Serhii <panchenko.mobi@gmail.com>
2024-10-17 20:58:39 +0200

    hw6 buil final

--- hw6/vite.config.ts
+++ hw6/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-home-work/hw5/dist/",
+  base: "/react-home-work/hw6/dist/",
 })
