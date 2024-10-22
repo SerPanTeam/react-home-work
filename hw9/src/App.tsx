ae0e73352802efec7d290f7c51dea57e5c91e693
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-10-22 15:24:22 +0200

    hw9

---
+++ hw9/src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <main className='container' style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <article>
+        <h1>Поиск</h1>
+      </article>
+    </main>
+  )
+}
+
+export default App
--- performance-app/eslint.config.mjs
+++ performance-app/eslint.config.mjs
@@ -1,13 +1,35 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        // Глобальные переменные Jest (если используете Jest для тестирования)
+        test: "readonly",
+        expect: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true }, // Перемещаем сюда
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "detect", // Автоматически определяет версию React
+      },
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
