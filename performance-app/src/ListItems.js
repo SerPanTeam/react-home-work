b9a7793d695e074709e060bd83e79a933a09300f
Panchenko Serhii <panchenko.mobi@gmail.com>
2024-10-22 12:37:34 +0200

    hw8

---
+++ performance-app/src/ListItems.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+
+const ListItems = () => {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  useEffect(() => {
+    console.log("Компонент ListItems обновлен");
+  }, []);
+
+  const addItem = () => {
+    if (inputValue.trim() !== "") {
+      setItems([...items, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={addItem}>Добавить</button>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListItems;
