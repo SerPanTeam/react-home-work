6e3552ae3e0052dd02661f4c6af8a7894e36e0ef
Panchenko Serhii <panchenko.serhii@hotmail.com>
2024-11-19 17:59:57 +0100

    MOD:HW15

--- hw15/src/App.tsx
+++ hw15/src/App.tsx
@@ -3,10 +3,12 @@ import ContactsIcon from '@mui/icons-material/Contacts';
 import UserList from './UserList';
 import UserForm from './UserForm';
 import { useState } from 'react';
+import { faker } from '@faker-js/faker';
+
 
 function App() {
 
-
+  const [usersArray, setusersArray] = useState([]); 
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
@@ -17,6 +19,11 @@ function App() {
     setOpen(false);
   };
 
+  const handleClickGenerate = () =>{
+    console.log(faker.person.fullName());
+    
+  }
+
   return (
     <>
       <CssBaseline />
@@ -24,6 +31,7 @@ function App() {
         <Toolbar>
           <ContactsIcon sx={{ mr: 2 }} />
           <Typography sx={{ flexGrow: 1 }}>Home work #15</Typography>
+          <Button color="inherit" onClick={handleClickGenerate}>Generate 1000 Users</Button>
           <Button color="inherit" onClick={handleClickOpen}>Add user</Button>
         </Toolbar>
       </AppBar>
--- hw15/src/UserList.tsx
+++ hw15/src/UserList.tsx
@@ -1,4 +1,6 @@
-import { Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import { IconButton, Paper, TableContainer, Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from '@mui/icons-material/Edit';
 
 function UserList() {
     return (
@@ -11,6 +13,8 @@ function UserList() {
                         <TableCell align="right">Email</TableCell>
                         <TableCell align="right">Phone</TableCell>
                         <TableCell align="right">Age</TableCell>
+                        <TableCell align="right">Action</TableCell>
+
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -21,16 +25,15 @@ function UserList() {
                         <TableCell align="right">2</TableCell>
                         <TableCell align="right">3</TableCell>
                         <TableCell align="right">4</TableCell>
+                        <TableCell align="right">
+                            <IconButton color="secondary">
+                                <EditIcon />
+                            </IconButton>
+                            <IconButton color="secondary">
+                                <DeleteForeverIcon />
+                            </IconButton>
+                        </TableCell>
                     </TableRow>
-
-                    <TableRow>
-                        <TableCell component="th" scope="row"> 0 </TableCell>
-                        <TableCell align="right">1</TableCell>
-                        <TableCell align="right">2</TableCell>
-                        <TableCell align="right">3</TableCell>
-                        <TableCell align="right">4</TableCell>
-                    </TableRow>
-
                 </TableBody>
             </Table>
         </TableContainer>)
